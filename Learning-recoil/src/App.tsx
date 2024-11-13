cdc1baa1605960e4bd36c14a32d0ac397425bf56
ayushjodd <ayushkalonia2016@gmail.com>
2024-11-13 12:59:45 +0530

    initial version withut state management

--- Learning-recoil/src/App.tsx
+++ Learning-recoil/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import ShimmerButton from "./components/Button";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState<number>(0);
 
+  function IncreaseCounter(random: number) {
+    setCount(random + 1);
+  }
+
+  function DecreaseCounter(random: number) {
+    setCount(random - 1);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen w-full bg-slate-700 flex flex-col items-center justify-center ">
+      <span className="text-white text-2xl mb-4">{count.toString()}</span>
+      <div className="flex gap-2 mx-auto">
+        <ShimmerButton
+          onClick={() => {
+            IncreaseCounter(count);
+          }}
+        >
+          Increase Counter
+        </ShimmerButton>
+        <ShimmerButton
+          onClick={() => {
+            DecreaseCounter(count);
+          }}
+        >
+          Decrease Counter
+        </ShimmerButton>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
